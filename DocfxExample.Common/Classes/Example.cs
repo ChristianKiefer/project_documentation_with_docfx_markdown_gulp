5a483d49b0159fcc9a1361ccce2eaa10ea583afd
ChristianKiefer <46448963+ChristianKiefer@users.noreply.github.com>
2020-04-05 15:36:42 +0200

    Add files via upload

---
+++ DocfxExample.Common/Classes/Example.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// Beispiel Beschreibung
+/// </summary>
+namespace DocfxExample.Common.Classes
+{
+    /// <summary>
+    /// Beispiel Beschreibung
+    /// </summary>
+    class Example
+    {
+        /// <summary>
+        /// Beispiel Beschreibung
+        /// </summary>
+        public void test()
+        {
+
+        }
+    }
+}
