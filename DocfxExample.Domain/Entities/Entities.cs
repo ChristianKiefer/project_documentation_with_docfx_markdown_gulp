144d63aa82d5cdad108575d2dfdfae1f771411d3
ChristianKiefer <46448963+ChristianKiefer@users.noreply.github.com>
2020-04-05 15:39:11 +0200

    Add files via upload

---
+++ DocfxExample.Domain/Entities/Entities.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// Beispiel Beschreibung
+/// </summary>
+namespace DocfxExample.Domain.Entities
+{
+    /// <summary>
+    /// Beispiel Beschreibung
+    /// </summary>
+    class Entities
+    {
+        /// <summary>
+        /// Beispiel Beschreibung
+        /// </summary>
+        public void test()
+        {
+
+        }
+    }
+}
