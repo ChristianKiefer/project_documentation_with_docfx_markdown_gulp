3bd79a28f2e90bf90fc9977f2c0c8316fada4f12
ChristianKiefer <46448963+ChristianKiefer@users.noreply.github.com>
2020-04-05 15:42:09 +0200

    Add files via upload

---
+++ DocfxExample.Infrastructure/SmtpService/SmtpService.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// Beispiel Beschreibung
+/// </summary>
+namespace DocfxExample.Infrastructure.SmtpService
+{
+    /// <summary>
+    /// Beispiel Beschreibung
+    /// </summary>
+    public class SmtpService
+    {
+        /// <summary>
+        /// Beispiel Beschreibung
+        /// </summary>
+        public void test()
+        {
+
+        }
+    }
+}
---
+++ DocfxExample.Infrastructure/obj/xdoc/cache/obj/hce0udfm.iju
@@ -0,0 +1,10 @@
+id: DocfxExample.Infrastructure
+language: CSharp
+name:
+  Default: DocfxExample.Infrastructure
+qualifiedName:
+  Default: DocfxExample.Infrastructure
+type: Assembly
+modifiers: {}
+items: []
+references: {}
