7a8d72dc68411c92d62a58df23ca3d853a80bea6
ChristianKiefer <46448963+ChristianKiefer@users.noreply.github.com>
2020-04-05 15:56:50 +0200

    Add files via upload

---
+++ DocfxExample.RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocfxExample.RazorPages.Pages
+{
+    /// <summary>
+    /// Beispiel Beschreibung
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        /// <summary>
+        /// Beispiel Beschreibung
+        /// </summary>
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ DocfxExample.RazorPages/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+/// <summary>
+/// Beispiel Beschreibung
+/// </summary>
+namespace DocfxExample.RazorPages
+{
+    /// <summary>
+    /// Beispiel Beschreibung
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Beispiel Beschreibung
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// Beispiel Beschreibung
+        /// </summary>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
