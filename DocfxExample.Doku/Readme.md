e3fcdc07315e5f515404009e1d018584ea75f7c0
ChristianKiefer <46448963+ChristianKiefer@users.noreply.github.com>
2020-04-05 16:02:15 +0200

    Add files via upload

---
+++ DocfxExample.Doku/Readme.md
@@ -0,0 +1,30 @@
+﻿# Project Documentation
+---------------------
+
+## Overview
+
+This project is used to create a documentation on the fly. To manage this we use the nuget package docfx.console:  
+https://dotnet.github.io/docfx/tutorial/docfx_getting_started.html#2-use-docfx-exe-directly     
+Doku for docfx.json:   
+https://dotnet.github.io/docfx/tutorial/docfx.exe_user_manual.html#3-docfxjson-format
+
+-----------------------   
+
+**How to build the documentation:**  
+* just build this project :-)
+
+**How to read this documentation**  
+
+1. open the folder "__site"_ in windows file explorer
+2. run the file index.html
+
+**Usefull tools**  
+It is usefull to install the Markdown Editor for better reading and editing the markdown files which are used in the documentation:  
+https://marketplace.visualstudio.com/items?itemName=MadsKristensen.MarkdownEditor
+
+Markdown syntax will be find here:  
+https://www.markdownguide.org/basic-syntax/
+
+
+ 
+
---
+++ DocfxExample.Doku/gulpfile.js
@@ -0,0 +1,31 @@
+﻿/// <binding BeforeBuild='copy:ReadMeFiles' />
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+
+
+gulp.task('copy:ReadMeFiles', async () => {
+    gulp.src('../DocfxExample.RazorPages/Readme.md')
+        .pipe(rename("ReadMeRazorPages.md"))
+        .pipe(gulp.dest('./articles'));
+    gulp.src('../DocfxExample.Common/Readme.md')
+        .pipe(rename("ReadMeCommon.md"))
+        .pipe(gulp.dest('./articles'));
+    gulp.src('../DocfxExample.Application/Readme.md')
+        .pipe(rename("ReadMeApplication.md"))
+        .pipe(gulp.dest('./articles'));
+    gulp.src('../DocfxExample.Domain/Readme.md')
+        .pipe(rename("ReadMeDomain.md"))
+        .pipe(gulp.dest('./articles'));
+    gulp.src('../DocfxExample.Infrastructure/Readme.md')
+        .pipe(rename("ReadMeInfrastructure.md"))
+        .pipe(gulp.dest('./articles'));
+    gulp.src('../DocfxExample.SqlServer/Readme.md')
+        .pipe(rename("ReadMeSqlServer.md"))
+        .pipe(gulp.dest('./articles'));
+    gulp.src('../DocfxExample.ApplicationTests/Readme.md')
+        .pipe(rename("ReadMeApplicationTests.md"))
+        .pipe(gulp.dest('./articles'));
+    gulp.src('../DocfxExample.InfrastructureTests/Readme.md')
+        .pipe(rename("ReadMeInfrastructureTests.md"))
+        .pipe(gulp.dest('./articles'));
+});
